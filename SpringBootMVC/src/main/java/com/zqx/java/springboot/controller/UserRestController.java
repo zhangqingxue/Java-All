a40e58ed83d252b33db4a5b312819c7b9ea512cb
zhangqingxue <1452428955@qq.com>
2019-08-12 11:48:13 +0800

    emmmm

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UserRestController.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UserRestController.java
@@ -3,6 +3,7 @@ package com.zqx.java.springboot.controller;
 import com.zqx.java.springboot.entity.User;
 import com.zqx.java.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,15 @@ public class UserRestController {
     public User login(String userName, String password) {
         User user = userService.getUser(userName, password);
         return user;
+    }
+
+    /**
+     * 占位符的方式传值
+     * 前端传递：api/v1/user/lgoin/ZHANG = /login/{userName}
+     */
+    @RequestMapping("/login/{userName}")
+    public User login(@PathVariable String userName) {
+        return null;
     }
 
 }
