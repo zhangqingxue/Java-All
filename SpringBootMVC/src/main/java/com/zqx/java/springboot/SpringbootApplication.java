1c72bdd743faf7d42f9aaf09150387cded06ac8a
zhangqingxue <1452428955@qq.com>
2019-08-12 14:45:49 +0800

    emmmm

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/SpringbootApplication.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/SpringbootApplication.java
@@ -2,7 +2,11 @@ package com.zqx.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync  // 异步注解
+@EnableScheduling  // 定时任务注解
 @SpringBootApplication
 public class SpringbootApplication {
 
---
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/core/TaskSchedulingServer.java
@@ -0,0 +1,34 @@
+package com.zqx.java.springboot.core;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskSchedulingServer {
+
+    @Value("${scheduled.executeIfDefault}")
+    boolean executeIfDefault;
+
+    @Async
+    @Scheduled(cron="${scheduled.executeCron}")
+    public void execute(){
+        System.out.println("=====执行开始======");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("<<<<<<<<<<<执行结束>>>>>>>>>>");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
