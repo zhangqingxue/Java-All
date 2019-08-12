8927f30d383b198c9c32115fbb68f2832ec0f024
zhangqingxue <1452428955@qq.com>
2019-08-12 13:44:12 +0800

    emmmm

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UserRestController.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UserRestController.java
@@ -5,7 +5,6 @@ import com.zqx.java.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
