854675623133c7ad349f2ed9f452a38fedddce90
zhangqingxue <1452428955@qq.com>
2019-08-26 20:05:12 +0800

    全局捕获Validation注解异常Exception

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/core/CustomExceptionHandler.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/core/CustomExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.zqx.java.springboot.core;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -12,21 +19,24 @@ public class CustomExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    Response handleException(Exception e) {
+    Response exceptionHandler(Exception e) {
         System.out.println(e.getMessage());
-        Response response = new Response(Response.NORMAL, e.getMessage());
+        Response response = new Response(Response.REQUEST_ERROR, e.getMessage());
         return response;
 
     }
 
-//    /**
-//     * 处理所有业务异常
-//     */
-//    @ExceptionHandler(BusinessException.class)
-//    @ResponseBody
-//    String handleBusinessException(BusinessException e){
-//        System.out.println("BusinessException");
-//        return "BusinessException";
-//    }
+    /**
+     * 处理所有validation注解异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    Response validExceptionHandler(MethodArgumentNotValidException  e){
+        System.out.println("MethodArgumentNotValidException");
+        FieldError error = e.getBindingResult().getFieldErrors().get(0);
+        System.out.println(error.getDefaultMessage());
+        Response response = new Response(Response.REQUEST_ERROR, error.getDefaultMessage());
+        return response;
+    }
 
 }
