4a9cfd657e46da9fee033459d886ac8cca9465d0
zhangqingxue <1452428955@qq.com>
2019-08-26 15:42:45 +0800

    emmmm

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UploadRestController.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/controller/UploadRestController.java
@@ -5,12 +5,10 @@ import com.github.tobato.fastdfs.domain.fdfs.StorePath;
 import com.github.tobato.fastdfs.domain.upload.FastImageFile;
 import com.github.tobato.fastdfs.domain.upload.ThumbImage;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
-import com.zqx.java.springboot.entity.User;
 import com.zqx.java.springboot.utils.DateUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -43,7 +41,8 @@ public class UploadRestController {
      * @param thumbnailIfDefault 0=不产生缩略图，1=产生缩略图并上传 默认0
      */
     @RequestMapping(value = "/upload/images", method = RequestMethod.POST)
-    public void uploadImages(MultipartFile file, @Min(0)@Max(1) int thumbnailIfDefault) {
+    public void uploadImages(MultipartFile file, @Min(0) @Max(1) @NotNull Integer thumbnailIfDefault) {
+        System.out.println(thumbnailIfDefault);
         try {
             InputStream inputStream = file.getInputStream();
             int fileSize = file.getBytes().length;
@@ -68,7 +67,7 @@ public class UploadRestController {
      * @param filePath StorePath.getFullPath() = group1/M00/00/00/wKjHil1fm_eAL-obAAh53vCw0rs686.png
      */
     @RequestMapping("/delete")
-    public void deleteImage(String filePath){
+    public void deleteImage(String filePath) {
 
         /** 此处获取存储位置信息 */
 
