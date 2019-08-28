b1b7fbbc9f138fd4efe55240e52dbe3738affca9
zhangqingxue <1452428955@qq.com>
2019-08-28 16:02:27 +0800

    AOP

--- SpringBootMVC/src/main/java/com/zqx/java/springboot/SpringbootApplication.java
+++ SpringBootMVC/src/main/java/com/zqx/java/springboot/SpringbootApplication.java
@@ -1,14 +1,23 @@
 package com.zqx.java.springboot;
 
+import com.zqx.java.springboot.core.DefaultAspectJProxyFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync  // 异步注解
 @EnableScheduling  // 定时任务注解
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringbootApplication {
+
+    @Bean
+    public DefaultAspectJProxyFactory createPorxy(){
+        return new DefaultAspectJProxyFactory();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
